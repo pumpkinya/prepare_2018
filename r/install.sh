3d391448e9f12ecc65550dbd0bbbc69601ac9987
IvanWoo22 <33291886+IvanWoo22@users.noreply.github.com>
2018-04-17 15:49:04 +0800

    Create install.sh

---
+++ r/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+hash Rscript 2>/dev/null || {
+    brew install r
+}
+
+cd ${BASE_DIR}
+
+Rscript install.R
+
+cat <<EOF > .Rprofile
+# set R mirror
+local({r <- getOption("repos")
+   r["CRAN"] <- "https://mirrors.tuna.tsinghua.edu.cn/CRAN"
+   options(repos=r)
+})
+EOF
+
+Rscript extrafont.R
+
+rm .Rprofile
