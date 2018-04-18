9de6169d0fa01c297e79edf6a47962a5f770c2ba
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2018-04-18 10:18:47 +0800

    Update 6-download.sh

--- 6-download.sh
+++ 6-download.sh
@@ -19,37 +19,8 @@ mkdir -p $HOME/share
 
 
 echo "==> kent bin"
-cd $HOME/bin/
-if [[ $(uname) == 'Darwin' ]];
-then
-    JKBIN_TAR_GZ=$HOME/prepare/resource/jkbin-darwin-2011.tar.gz
-    if [ ! -e ${JKBIN_TAR_GZ} ];
-    then
-        wget -N https://github.com/egateam/egavm/releases/download/20170907/jkbin-darwin-2011.tar.gz
-    fi
-else
-    JKBIN_TAR_GZ=$HOME/prepare/resource/jkbin-ubuntu-1404-2011.tar.gz
-    if [ ! -e ${JKBIN_TAR_GZ} ];
-    then
-        wget -N https://github.com/egateam/egavm/releases/download/20170907/jkbin-ubuntu-1404-2011.tar.gz
-    fi
-fi
-echo "==> untar from ${JKBIN_TAR_GZ}"
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtChain
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtSort
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtToMaf
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainAntiRepeat
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainMergeSort
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainNet
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainPreNet
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainSplit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainStitchId
-tar xvfz ${JKBIN_TAR_GZ} x86_64/faToTwoBit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/lavToPsl
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netChainSubset
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netFilter
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netSplit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netSyntenic
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netToAxt
-
-mv $HOME/bin/x86_64/* $HOME/bin/
+for f in faToTwoBit axtChain chainAntiRepeat lavToPsl chainMergeSort chainPreNet chainNet netSyntenic netChainSubset chainStitchId netSplit netToAxt axtSort axtToMaf netFilter chainSplit; do
+      wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/${f};
+      mv ./${f} $HOME/bin/;
+      chmod +x $HOME/bin/${f};
+done
